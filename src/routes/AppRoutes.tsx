9b019ed0b6a0a90c658b3c87c07faec97dda3100
Darlan Fabricio <darlanfabricio@gmail.com>
2022-12-17 15:54:43 -0300

    first commit

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Home from '../pages/Home';
+import Home from '../pages/OldHome';
 import ListNote from '../pages/ListNote';
 import Login from '../pages/Login';
 import Register from '../pages/Register';
